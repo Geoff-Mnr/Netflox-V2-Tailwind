b87ef37dc2f642a70998a6daecbba8ccd96c0425
Yakuz0r <geoffrey_meunier@live.be>
2023-03-29 20:56:20 +0200

    add files css tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
+    colors:{
+      'red-netflix': '#fd0000',
+      'black':'#000000',
+      'white':'#f3f3f3',
+      'grey':'#222222',
+      'l-grey':'#b79f9f',
+      'blue':'#0071eb',
+    },
     extend: {},
   },
   plugins: [],
